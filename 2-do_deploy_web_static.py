9b9456cf57830d6e083785d35a31ea97b69462a6
abdulnasirym <abdulnasiry05@gmail.com>
2024-07-15 13:31:38 -0700

    task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,13 +7,14 @@ archive to the web servers
 from fabric.api import put, run, env
 import os
 
+
 env.hosts = ['34.229.161.126', '54.146.59.87']
 env.user = "ubuntu"
 env.key_filename = "~/.ssh/id_rsa"
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to the web servers"""
+    """distributes an archive to the web servers"""
     if not os.path.exists(archive_path):
         return False
     try:
@@ -21,22 +22,15 @@ def do_deploy(archive_path):
         no_ext = file_n.split(".")[0]
         path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        run(f'mkdir -p {path}{no_ext}/')
-        run(f'tar -xzf /tmp/{file_n} -C {path}{no_ext}/')
-        run(f'rm /tmp/{file_n}')
-
-        # Remove existing subdirectories if they exist
-        run(f'rm -rf {path}{no_ext}/web_static')
-
-        # Move files from the web_static directory to the release folder
-        run(f'mv {path}{no_ext}/web_static/* {path}{no_ext}/')
-
-        run(f'rm -rf {path}{no_ext}/web_static')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
-        run(f'ln -s {path}{no_ext}/ /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        print("Changes Deployed")
         return True
-    except Exception as e:
-        print(f"Deployment failed: {e}")
+    except:
         return False
-
 
