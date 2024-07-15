7c7e38ec7276d26638dd50283643d06b1d394321
abdulnasirym <abdulnasiry05@gmail.com>
2024-07-15 08:08:34 -0700

    task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -15,19 +15,6 @@ env.user = "ubuntu"
 env.key_filename = "~/.ssh/id_rsa"
 
 
-def do_pack():
-    """Fabric script that generates a .tgz archive from the contents of the...
-    ...web_static folder
-    """
-    local("sudo mkdir -p versions")
-    date = datetime.now().strftime("%Y%m%d%H%M%S")
-    filename = f"versions/web_static_{date}.tgz"
-    result = local(f"sudo tar -cvzf {filename} web_static")
-    filesize = os.path.getsize(filename)
-    if result.succeeded:
-        print(f"web_static packed: {filename} -> {filesize}Bytes")
-        return filename
-
 def do_deploy(archive_path):
     # checks if if path exist
     if not os.path.exists(archive_path):
@@ -35,23 +22,18 @@ def do_deploy(archive_path):
     try:
         # upload archive to /tmp/
         put(archive_path, "/tmp/")
-
         # creating the proper directory to extract to
         file_name = archive_path.split("/")[-1]
         folder_name = file_name.split(".")[0]
         run(f"mkdir -p /data/web_static/releases/{folder_name}/")
-
         # uncompressing to the directory
         run(f"tar -xzf /tmp/{file_name} -C /data/web_static/releases/{folder_name}/")
-
         # deleting the archive from the server
         run(f"rm /tmp/{file_name}")
-
         # moving the extracted files from the archive to the folder
         run(f"mv /data/web_static/releases/{folder_name}/web_static/* \
         /data/web_static/releases/{folder_name}/")
         run(f"rm -rf /data/web_static/releases/{folder_name}/web_static/")
-
         # Remove the current symbolic link and create a new one
         run("rm -rf /data/web_static/current")
         run(f"ln -s /data/web_static/releases/{folder_name}/ /data/web_static/current")
