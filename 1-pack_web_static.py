4bfbac73121a154d769aa9995742caf651a4ea27
abdulnasirym <abdulnasiry05@gmail.com>
2024-07-15 03:38:19 -0700

    task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,17 +5,17 @@ from the contents of the web_static folder
 of your AirBnB Clone repo
 """
 
-from fabric.api import *
+from fabric.api import local
 from datetime import datetime
 import os
 
 def do_pack():
 
-    local("sudo mkdir -p versions")
+    local("mkdir -p versions")
     t =datetime.now()
     t_str = t.strftime("%Y%m%d%H%M%S")
 
-    local(f"sudo tar -cvzf versions/web_static_{t_str}.tgz web_static")
+    local(f"tar -cvzf versions/web_static_{t_str}.tgz web_static")
 
     f_path = f"versions/web_static_{t_str}.tgz"
     f_size = os.path.getsize(f_path)
