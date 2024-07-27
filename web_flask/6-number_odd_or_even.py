f8dd94ed0c058938eaa664b87c93fa3c85dbb93c
abdulnasirym <abdulnasiry05@gmail.com>
2024-07-27 10:16:45 -0700

    task 6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -49,5 +49,6 @@ def numbersandevenness(n):
     """display a HTML page only if n is an integer"""
     return render_template('6-number_odd_or_even.html', n=n)
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
